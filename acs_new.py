c06e7188044bce66e9ec067c8c0b46e540ed5340
kpark <sucream1004@naver.com>
2019-04-18 09:03:17 -0400

    jfaisd

--- acs_new.py
+++ acs_new.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# NYC ACS data loader (based on census tract)
 import os
-import sys
 
 import numpy as np
 import pandas as pd
 import geopandas as gpd
 from shapely import wkt
-
 import matplotlib.pyplot as plt
+
+# variables from ACS API
+print('Loading variables')
 df = pd.read_json('https://api.census.gov/data/2017/acs/acs5/variables.json')
 df = df.reset_index()
-print('Loading the variables')
+print('Variables loaded')
 
 var_names = []
 for var,f in zip(df['index'], df['variables']):
@@ -21,7 +23,17 @@ for var,f in zip(df['index'], df['variables']):
     except:
         continue
 
-var = sys.argv[1]
+# Searching relevant code
+word = input('Enter word to search (lower case): ')
+
+df1 = pd.DataFrame(columns=['var','des1','des2'])
+for i in range(len(var_names)):
+    if word in var_names[i][2].lower() and var_names[i][0] != 'GEO_ID':
+        df1 = df1.append({'var': var_names[i][0],'des1': var_names[i][1],'des2': var_names[i][2] }, ignore_index=True)
+
+print(df1)
+var = input('Select code from the list: ')
+# 
 url = 'https://api.census.gov/data/2017/acs/acs5?get={},NAME&for=tract:*&in=state:36%20county:*'.format(var)
 print('Loading {} data'.format(var))
 dat = pd.read_json(url)
@@ -30,11 +42,6 @@ dat.columns = col_name
 dat.drop(dat.head(1).index, inplace=True)
 
 dat['cd'] = dat.state + dat.county + dat.tract
-
-for i in range(len(var_names)):
-    if sys.argv[1] in var_names[i][0]:
-        print(var_names[i])
-        ti = var_names[i]
 
 ct2010 = pd.read_csv('https://data.cityofnewyork.us/api/views/i69b-3rdj/rows.csv?accessType=api', dtype=str)
 
